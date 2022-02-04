a212e091515644f881f2bbc8275cebd4c5547142
Ayu Dariah Sakinah <93211428+ayusakinah@users.noreply.github.com>
2022-02-04 22:20:17 +0700

    Push Ienyen, Riana

--- camember.js
+++ camember.js
@@ -5,8 +5,8 @@ while (input) {
 
   // Menangkap data nama terpilih
   var lolos = {
-    robotic: ['Dadan Hidayatuloh', 'Stephanie Claudia', 'Lenyen Nandha Galuhsusan', 'Joel Pangaribuan'],
-    scientificResearch: ['Muhammad Rizal Nurromdhoni', 'Reynaldi Ariza'],
+    robotic: ['Dadan Hidayatuloh', 'Stephanie Claudia', 'Ienyen Nandha Galuhsusan', 'Joel Pangaribuan'],
+    scientificResearch: ['Muhammad Rizal Nurromdhoni', 'Reynaldi Ariza', 'Riana Ayu Noviana'],
     dataScience: ['Sugih Pratama Nugraha', 'Adib Danu Wirawan', 'Nanda Putra Niko Rizaldi', 'Selly Novianti Putri Permana', 'Rifqi Asril Nurrachman'],
     webDev: ['Yohannes Mangaradot', 'Fadhly Naufandy Lathif', 'M Syahrul Amin'],
   };
@@ -14,7 +14,7 @@ while (input) {
   var result = '';
   if (p == lolos.robotic[0] || p == lolos.robotic[1] || p == lolos.robotic[2] || p == lolos.robotic[3]) {
     result = "Congratulation you're a member of Robotic PRC 2022, " + p + ' !';
-  } else if (p == lolos.scientificResearch[0] || p == lolos.scientificResearch[1]) {
+  } else if (p == lolos.scientificResearch[0] || p == lolos.scientificResearch[1] || p == lolos.scientificResearch[2]) {
     result = "Congratulation you're a member of Scientific Research PRC 2022, " + p + ' !';
   } else if (p == lolos.dataScience[0] || p == lolos.dataScience[1] || p == lolos.dataScience[2] || p == lolos.dataScience[3] || p == lolos.dataScience[4]) {
     result = "Congratulation you're a member of Data Science PRC 2022, " + p + ' !';
